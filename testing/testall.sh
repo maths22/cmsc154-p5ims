22947e8dacbe837b5171d3fe4ef01f5ea043a89f
David Lax <dlax@uchicago.edu>
2015-06-06 21:19:45 +0000

    additional testing script with output
    git-svn-id: https://phoenixforge.cs.uchicago.edu/svn/burroughsjw-dlax-cs154-2015@63 51d68498-dea4-41ab-9702-c75bc961922d

---
+++ testing/testall.sh
@@ -0,0 +1,2 @@
+(touch log.txt; rm log.txt; ./01-db.sh; ./02-register.sh; ./03-login.sh; ./04-logout.sh; ./05-friendadd.sh; ./06-friendrm.sh; ./07-friendstat1.sh;
+./08-friendstat2.sh; ./09-friendim.sh; rm log.txt; ./10-errors.sh) | grep "P5IMS TEST "
